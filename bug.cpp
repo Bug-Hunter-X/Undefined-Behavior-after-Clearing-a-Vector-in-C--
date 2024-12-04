c3b46380c977eaa02143a7c5045cef0956ef0bcc
BugHunter-X <askzenai@gmail.com>
2024-12-04 02:30:37 +0100

    Add bug.cpp file

---
+++ bug.cpp
@@ -0,0 +1,1 @@
+std::vector<int> vec; for (int i = 0; i < 10; ++i) { vec.push_back(i); } for (int i = 0; i < vec.size(); ++i) { std::cout << vec[i] << " "; } std::cout << std::endl; vec.clear(); //This line is the bug. for (int i = 0; i < vec.size(); i++) { std::cout << vec[i] << " "; } std::cout << std::endl; 
